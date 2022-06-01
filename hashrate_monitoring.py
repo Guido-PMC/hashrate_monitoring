eec82046ffc85b12e1af72d8a3be25283a925a08
guigonzalez <guidogermang@gmail.com>
2022-06-01 16:12:15 -0300

    Agrego[:5] al payload

--- hashrate_monitoring.py
+++ hashrate_monitoring.py
@@ -33,7 +33,7 @@ for x in list_miners:
     print ("Hashrate: "+str(x.hash))
     print("Hostname: "+str(hostname))
     url = "http://45.239.131.173:5000/wallet/"+x.wallet
-    payload="{\""+str(hostname)+"\" : \""+str(x.hash)+"\"}"
+    payload="{\""+str(hostname)[:5]+"\" : \""+str(x.hash)+"\"}"
     headers = {
         'Content-Type': 'application/json'
         }
